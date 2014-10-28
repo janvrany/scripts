2a738d22e571e5502eadb80ce55fee143f6ea7fd
Jan Vrany <jan.vrany@fit.cvut.cz>
2014-10-28 21:39:40 +0000

    Finished patch-and-check.rb. REAME.md updated.

--- README.md
+++ README.md
@@ -4,17 +4,24 @@ and maintenance. May or may not be useful for other people :-)
 Scripts
 -------
 
-* *bisect.rb*: a tool to find a minimal set of files causing a problem. Similar to `git bisect` or `hg bisect` but usable standalone and even for non-git, non-hg sources.
+* *[bisect.rb][1]*: a tool to find a minimal set of files causing a problem. Similar to `git bisect` or `hg bisect` but usable standalone and even for non-git, non-hg sources.
 
-* *stx-pkg-rename.rb*: renaming a Smalltalk/X package is sort of a pain. This script makes it a little bit easier.
+* *[patch-and-check.rb][2]*: a simple tool to apply a set of patches and verify patched sources. Handy when transplating changes from one branch to another and SCM is not of much help (such as cross-SCM)
 
-* *hg-archive-revisions.rb*: an `hg archive` wrapper which allows to export multiple revisions at once, in the same way `hg export` can. Handy for generating complete archives for those who don't like patches :-)
+* *[stx-pkg-rename.rb][3]*: renaming a Smalltalk/X package is sort of a pain. This script makes it a little bit easier.
+
+* *[hg-archive-revisions.rb][4]*: an `hg archive` wrapper which allows to export multiple revisions at once, in the same way `hg export` can. Handy for generating complete archives for those who don't like patches :-)
 
 You can find more details about inside these scripts, or run script with `--help`.
-More scripts will come as I polish them.
+More scripts will come as I polish them :-)
 
 Installation
 ------------
 
 Each script is standalone, so just copy it wherever convenient for you and run it.
 However, you may need to install Ruby to run Ruby scripts (those with `.rb` suffix)
+
+[1]: https://bitbucket.org/janvrany/jv-scripts/src/tip/bisect.rb
+[2]: https://bitbucket.org/janvrany/jv-scripts/src/tip/patch-and-check.rb
+[3]: https://bitbucket.org/janvrany/jv-scripts/src/tip/stx-pkg-rename.rb
+[4]: https://bitbucket.org/janvrany/jv-scripts/src/tip/hg-archive-revisions.rb
--- patch-and-check.rb
+++ patch-and-check.rb
@@ -1,26 +1,39 @@
 #!/usr/bin/ruby
 DOCUMENTATION = <<DOCEND
-A simple script to apply a sequence of .patch files and after each,
+A simple script to apply a sequence of .patch files and after each
 check if it applied cleanly.
 
-Useful when you have two eventually divegring lines of development in two
+Useful when you have two possibly diverging lines of development in two
 different SCMs and want to transpant fixes done in one line on top of a
 head in the other (such as transplating changes from HG to on top of CVS HEAD)
 
 The check script (if provided) is passed a path to working directory and must
 return exit status 0 if working copy is good of nonzero if its bad.
 
-There's two "built-in" check scripts:
+There are two "built-in" check scripts:
 
   * "internal:make" (the default, if no check script is used) which just runs
      a make. A patch is consireded good if make proceeds without errors.
   * "internal:none" a patch is slways consireded good (as long as patching
      proceeds fine)
 
+Optionally an archive is saved after each patch is applied and validated,
+in which case the name of the file is given using a format string. The
+formatting rules are as follows:
+
+    "%%"          literal "%" character
+    "%1"          zero-padded number `1`
+    "%n"          zero-padded sequence number, starting at 1
+    "%N"          number of patches being applied
+    "%P"          basename of patch file without last suffix.
+
+Examples:
+
+
 See also:
 
-* hg export
-* hg import
+  * hg export
+  * hg import
 
 DOCEND
 
@@ -47,7 +60,9 @@ def check(check_script, dir = $DIR_WORK)
 end
 
 def check_internal_make(dir)
-    return (system "make -C #{dir} clean") && (system "make -C #{dir}")
+    retval = (system "make -C #{dir} clean") && (system "make -C #{dir}")
+    system "make -C #{dir} clean"
+    return retval
 end
 
 def check_external(check_script, dir)
@@ -56,7 +71,7 @@ end
 
 def patch_and_check(patch, patchlevel, check_script)
   if not File.exist? patch then
-    error "patch '#{patch}' does not exist"
+    error "Patch '#{patch}' does not exist"
   end
 
   puts
@@ -75,15 +90,57 @@ def patch_and_check(patch, patchlevel, check_script)
   end
 
   puts "Patch '#{patch}'' applied cleanly"
-
-
 end
 
+def archive(patch, patch_no, patches_total, fmt)
+  _fmt = fmt
+  _fmt = _fmt.gsub(/\%/, '%%')
+  _fmt = _fmt.gsub(/\%\%n/, '%{n}')
+  _fmt = _fmt.gsub(/\%\%N/, '%{N}')
+  _fmt = _fmt.gsub(/\%\%P/, '%{P}')
+
+
+  if patches_total < 10 then
+    _fmt = _fmt.gsub(/\%\%1/, '1')
+    fmt_n = "%01d"
+  elsif patches_total < 100 then
+    _fmt = _fmt.gsub(/\%\%1/, '01')
+    fmt_n = "%02d"
+  elsif patches_total < 1000 then
+    _fmt = _fmt.gsub(/\%\%1/, '001')
+    fmt_n = "%03d"
+  elsif patches_total < 10000 then
+    _fmt = _fmt.gsub(/\%\%1/, '0001')
+    fmt_n = "%04d"
+  else
+    _fmt = _fmt.gsub(/\%\%1/, '00001')
+    fmt_n = "%05d"
+  end
+
+  archive_file = _fmt % { :n => (fmt_n % patch_no),
+                          :N => (fmt_n % patches_total),
+                          :P => patch.sub(/\.[^\.]+$/,'').sub(/^.*\//,'') }
+  archive_dir = archive_file.sub(/\.zip$/,'').sub(/^.*\//,'')
+  puts "Archiving #{patch} to #{archive_file}"
+  Dir.mktmpdir do | tmpdir |
+    ln_s($DIR_WORK, File.join(tmpdir, archive_dir))
+    Dir.chdir tmpdir do
+      system 'ls'
+      zip_cmd = "zip -r #{archive_file} #{archive_dir} -x \"*.o\" -x \"*.obj\" -x \".orig\" -x \".rej\" -x \".#*\""
+      puts zip_cmd
+      if not system(zip_cmd) then
+        error("Cannot create archive '#{archive_file}'")
+      end
+    end
+  end
+end
 
 def main()
     check_script = 'internal:make'
-    patchlevel = '0';
-    pause = false
+    patchlevel = '1';
+    interactive = false
+    archive = false;
+    archive_fmt = nil
 
 
     optparse = OptionParser.new do | opts |
@@ -94,11 +151,15 @@ def main()
         opts.on('-c', "--check SCRIPT", "Path to script to check whether current working version is good or bad (default is to run make)") do | s |
             check_script = s
         end
-        opts.on('-p', "--strip NUM", "Strip  the  smallest  prefix containing num leading slashes from each file name found in the patch file. This option is passed at it is to `patch`") do | s |
+        opts.on('-p', "--strip NUM", "Strip  the  smallest  prefix containing num leading slashes from each file name found in the patch file. This option is passed at it is to `patch`. Default is '1'.") do | s |
             patchlevel = s
         end
-        opts.on(nil, '--pause', "Stop after each successfuly applied patch, waiting for user to confirm.") do
-            pause = true
+        opts.on('-i', '--interactive', "Stop after each successfuly applied patch, waiting for user to confirm.") do
+            interactive = true
+        end
+        opts.on('-a', '--archive FORMAT', "Export .zip archive of code after applying each patch.") do | s |
+            archive = true
+            archive_fmt = File.expand_path s
         end
 
         opts.on(nil, '--help', "Prints this message") do
@@ -112,9 +173,10 @@ def main()
 
     if not File.exist? $DIR_WORK then
       error("Working directory does not exist")
-    end
-    if not File.directory? $DIR_WORK then
+    elsif  not File.directory? $DIR_WORK then
       error("Working directory is not directory")
+    else
+      $DIR_WORK =  File.expand_path $DIR_WORK
     end
 
     patches = ARGV
@@ -126,15 +188,27 @@ def main()
       error "No patches specified"
     end
 
-    puts "Will apply following patches in order:"
-    patches.each do | patch |
-      puts patch
+    if interactive then
+      puts "Will apply following patches in order:"
+      for i in 0..patches.size - 1
+        patch = patches[i]
+        puts "#{i+1} #{patch.sub(/\.[^\.]+$/,'').sub(/^.*\//,'')}"
+      end
     end
 
     Dir.chdir($DIR_WORK) do
-      patches.each do | patch |
-        patch_and_check(patch,patchlevel, check_script)
-        if pause then
+      for i in 0..patches.size - 1 do
+        patch = patches[i]
+
+        patch_and_check(patch, patchlevel, check_script)
+        if archive then
+          archive(patch, i + 1, patches.size, archive_fmt)
+        end
+
+        if interactive then
+          if (i < (patches.size - 1)) then
+            puts "Next patch is #{patches[i+1]}"
+          end
           puts "Press enter key to continue (CTRL-C to abort)"
           ignored = STDIN.gets
         end
