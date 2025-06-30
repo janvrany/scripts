d7172ece6948ca8fac5ff9b1c00f8d0ca4469c7a
Jan Vrany <jan@vrany.io>
2025-06-30 11:39:49 +0100

    Improve `github-activity.py`

--- github-activity.py
+++ github-activity.py
@@ -28,10 +28,10 @@ repositories.
 
 Print activity summary for all repositories owned by user
 "johndoe" and also on repository "coolstuff" owned by "janedoe"
-for the past month (ending with today):
+from June 1st, 2035 to June 30th, 2035
 
     export GITHUB_TOKEN=ghp_ABCD....
-    github-sync.py --output /srv/backups/github.com \
+    github-activity.py --from 2025-06-01 --to 2025-06-30 \
         --user johndoe \
         --repo janedoe/coolstuff
 """
@@ -70,25 +70,25 @@ def activity_of(item, start, end):
     if (modified < start) or (modified > end):
         # There was no activity on given item, return
         return None
-    
+
     if (item.state == 'closed'):
         try:
             pull = item.as_pull_request()
         except: 
             pull = None
-        
+
         if pull is not None and pull.merged:
             return MERGED
         else:
             return CLOSED
-        
+
     if (created >= start):
         return CREATED
-    
+
     return UPDATED
 
 def process(repo, startDate, endDate, format):
-    items = []    
+    items = []
     for issue in repo.get_issues(state='all',since=datetime.combine(startDate, time())):
         activity = activity_of(issue, startDate, endDate)
         if activity is not None:
@@ -97,19 +97,14 @@ def process(repo, startDate, endDate, format):
     items.sort(key=lambda item : item[1].number)
 
     if len(items) > 0:
-        print("Repository %s \n" % repo.html_url)    
+        print("Repository %s \n" % repo.html_url)
         for item in items:
             print(format.format(activity=item[0], issue=item[1]))
         print("")
     return True
-    
+
 
 if __name__ == '__main__':
-    today = date.today()
-    monthago = date(today.year if today.month > 1 else today.year - 1,
-                    today.month if today.month > 1 else 12,
-                    today.day)
-
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--user", metavar="USER",
                         dest='users', action='append',
@@ -119,11 +114,9 @@ if __name__ == '__main__':
                         help="GitHub repository name to include in activity summary")
     parser.add_argument("--from", metavar="START_DATE",
                         dest='start',type=date.fromisoformat,
-                        default=monthago,
-                        help="Start of activity summary period as YYYY-MM-DD. Defaults to a month ago from today.")
+                        help="Start of activity summary period as YYYY-MM-DD. Defaults to a month ago from --to.")
     parser.add_argument("--to", metavar="END_DATE",
                         dest='end',type=date.fromisoformat,
-                        default=monthago,
                         help="End of activity summary period as YYYY-MM-DD. Defaults to today.")
     parser.add_argument("--format", metavar="FORMAT",
                         dest='format',
@@ -133,8 +126,8 @@ if __name__ == '__main__':
                         dest='format',action='store_const',
                         const=FMT_ONELINE,
                         help="Use short one-line format for activities.")
-    
-    
+
+
     options = parser.parse_args()
 
     if (not 'GITHUB_TOKEN' in environ):
@@ -150,6 +143,14 @@ if __name__ == '__main__':
     if len(options.repos) + len(options.users) == 0:
         log.error("No --repo or --user options specified")
         exit(1)
+
+    if options.end == None:
+        options.end = date.today()
+
+    if options.start == None:
+        options.start = date(options.end.year if options.end.month > 1 else options.end.year - 1,
+                    options.end.month - 1 if options.end.month > 1 else 12,
+                    options.end.day)
 
     try:
         gh = Github(environ['GITHUB_TOKEN'])
