9da34834e13c8a2ab91bdd630068a65a9eca6708
Jan Vrany <jan@vrany.io>
2025-10-14 17:03:24 +0100

    Add new option `--secret` to `get-oauth2-token.py`
    This commit adds an option to obtain refresh token from "secret service" (GNOME Keyring,
    KeepassXC and perhaps others). This is more convenient as it allows multiple
    applications (email client, git-send-email) to use the same refresh token and
    maybe more secure as the token is stored in app designed to store secrets rather
    than in files on the disk.
    
    For now however, it only works with refresh tokens stored by Evolution.

--- get-oauth2-token.py
+++ get-oauth2-token.py
@@ -14,16 +14,21 @@ such a Google may be added in a future if needed).
         virtualenv --prompt scripts .venv
         . .venv/bin/activate
 
- 2. Install `msal`:
+ 2. Install `msal` and `secretstorage`:
 
-        pip3 install msal
+        pip3 install msal secretstorage
 
 Alternatively:
 
-    apt-get install python3-msal
+    apt-get install python3-msal python3-secretstorage
 
 """
 
+#
+# Defaults
+#
+DEFAULT_OAUTH2_CLIENT_ID='20460e5d-ce91-49af-a3a5-70b6be7486d1'
+DEFAULT_OAUTH2_SCOPES=["https://outlook.office.com/IMAP.AccessAsUser.All", "https://outlook.office.com/SMTP.Send"]
 
 #
 # General debugging support.
@@ -34,6 +39,7 @@ Alternatively:
 import os
 import os.path
 import sys
+import json
 
 try:
     import ipdb
@@ -77,62 +83,51 @@ except ImportError:
     logging.error("Failed to import `msal`, please `pip3 install msal`")
     sys.exit(1)
 
+try:
+    import secretstorage
+except ImportError:
+    logging.error("Failed to import `secretstorage`, please `pip3 install secretstorage`")
+    sys.exit(1)
+
 
 
 class MicrosoftO365(object):
-    def __init__(self, config):
-        self.config = config
-        if not 'client_id' in config:
-            raise ValueError("No 'client_id' in config!")
-        elif not 'scopes' in config:
-            raise ValueError("No 'scope' in config!")
+    def __init__(self, client_id = DEFAULT_OAUTH2_CLIENT_ID, client_credential = None, scopes = DEFAULT_OAUTH2_SCOPES, token_cache_file = None):
 
-        self._cache = msal.SerializableTokenCache()
-        if self.config.get('cache') is not None:
-            if os.path.exists(config['cache']):
-                self._cache.deserialize(open(config['cache'], "r").read())
+        self._scopes = scopes
+        self._token_cache_file = token_cache_file
+        self._token_cache = msal.SerializableTokenCache()
+        if self._token_cache_file is not None:
+            if os.path.exists(self._token_cache_file):
+                self._token_cache.deserialize(open(self._token_cache_file, "r").read())
 
         self._app = msal.ConfidentialClientApplication(
-                            client_id = config.get('client_id'),
-                            client_credential = config.get('client_credential', None),
-                            token_cache = self._cache)
+                            client_id = client_id,
+                            client_credential = client_credential,
+                            token_cache = self._token_cache)
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if self._cache and self._cache.has_state_changed:
-            with open(config['cache'], "w") as cache_file:
-                cache_file.write(self._cache.serialize())
+        if self._token_cache_file is not None and self._token_cache.has_state_changed:
+            with open(self._token_cache_file, "w") as token_cache_io:
+                token_cache_io.write(self._token_cache.serialize())
 
-    def get_token(self, interactive = False):
+    def get_token_by_refresh_token(self, refresh_token, interactive = False):
         """
         Fetch and return access token as string.
 
-        If interactive is `False` and token cache is either not
-        configured or suitable refresh token is not there,
-        raise an exception.
+        If interactive is `False` refresh token is ivalid, raise an
+        exception.
 
         If interactive is `True`, guide user through the
         authentication/authorization process.
         """
-
-        reply = self._app.acquire_token_silent(config["scopes"], account=None)
-        if not reply:
-            # Extract the refresh token manually, sigh
-            refresh_tokens = self._cache.find('RefreshToken')
-            if len(refresh_tokens) > 0:
-                refresh_token = refresh_tokens[0]['secret']
-                reply = self._app.acquire_token_by_refresh_token(refresh_token=refresh_token, scopes=config["scopes"])
-                if reply and "error" in reply:
-                    if "suberror" in reply and reply["suberror"] in ('token_expired', 'bad_token'):
-                        logging.info("Refresh token expired")
-                        reply = None
-                    else:
-                        raise Exception(reply["error_description"])
+        reply = self._app.acquire_token_by_refresh_token(refresh_token=refresh_token, scopes=self._scopes)
         if not reply:
             if interactive:
-                auth_request = self._app.initiate_auth_code_flow(scopes = config.get('scopes'), redirect_uri = config.get('redirect_uri', "https://login.microsoftonline.com/common/oauth2/nativeclient"))
+                auth_request = self._app.initiate_auth_code_flow(scopes = self._scopes, redirect_uri = 'https://login.microsoftonline.com/common/oauth2/nativeclient')
                 auth_uri = auth_request['auth_uri']
                 print(f"""
 Please open folowing URL in a browser...
@@ -148,7 +143,7 @@ address bar here (and press enter):""")
                     key, value = key_and_value.split('=')
                     auth_response[key] = value
 
-                reply = self._app.acquire_token_by_auth_code_flow(auth_code_flow=auth_request, auth_response=auth_response, scopes = config.get('scopes'))
+                reply = self._app.acquire_token_by_auth_code_flow(auth_code_flow=auth_request, auth_response=auth_response, scopes = self._scopes)
                 if "error" in reply:
                     raise Exception(reply["error_description"])
             else:
@@ -156,6 +151,54 @@ address bar here (and press enter):""")
         if not 'access_token' in reply:
             raise Exception('Oops, no access token in reply!')
         return reply['access_token']
+
+    def get_token(self, interactive = False):
+        """
+        Fetch and return access token as string.
+
+        If interactive is `False` and token cache is either not
+        configured or suitable refresh token is not there,
+        raise an exception.
+
+        If interactive is `True`, guide user through the
+        authentication/authorization process.
+        """
+
+        reply = self._app.acquire_token_silent(self._scopes, account=None)
+        if reply:
+            access_token = reply['access_token']
+        else:
+            # Extract the refresh token manually, sigh
+            refresh_tokens = self._token_cache.find('RefreshToken')
+            if len(refresh_tokens) > 0:
+                refresh_token = refresh_tokens[0]['secret']
+            else:
+                refresh_token = 'BOGUS'
+            access_token = self.get_token_by_refresh_token(refresh_token)
+        return access_token
+
+    def get_token_by_refresh_token_in_secret(self, secret_title, interactive = False):
+        """
+        Fetch and return access token as string using refresh token stored
+        in secret name secret_title.
+
+        If interactive is `False` and refresh token invalid,
+        raise an exception.
+
+        If interactive is `True` and refresh token invalid, guide user
+        through the authentication/authorization process.
+
+        CAVEAT: For now, it only supports refresh token stored in Evolution
+        format.
+        """
+        secrets = secretstorage.get_default_collection(secretstorage.dbus_init())
+        refresh_token = None
+        for secret in secrets.search_items({ 'Title' : secret_title }):
+            refresh_token_json = json.loads(secret.get_secret().decode('utf8'))
+            refresh_token = refresh_token_json['refresh_token']
+        if refresh_token is None:
+            raise Exception("No such secret: %s" % secret_title)
+        return self.get_token_by_refresh_token(refresh_token, interactive)
 
     def get_username(self):
         accounts = self._app.get_accounts()
@@ -177,18 +220,21 @@ if __name__ == '__main__':
     parser.add_argument("-o", "--output", metavar="FILE",
                         dest='output', default='-',
                         help="Where to write access token, defaults to - (stdout)")
-    parser.add_argument("--client", metavar="CLIENT_ID",
-                        dest='client_id', default='20460e5d-ce91-49af-a3a5-70b6be7486d1',
-                        help="Client (application) ID, defaults to 'GNOME Evolution` (20460e5d-ce91-49af-a3a5-70b6be7486d1)")
-    parser.add_argument("--secret", metavar="CLIENT_SECRET",
+    parser.add_argument("--client_id", metavar="CLIENT_ID",
+                        dest='client_id', default=DEFAULT_OAUTH2_CLIENT_ID,
+                        help="Client (application) ID, defaults to 'GNOME Evolution` (%s)" % DEFAULT_OAUTH2_CLIENT_ID)
+    parser.add_argument("--client_secret", metavar="CLIENT_SECRET",
                         dest='client_credential', default=None,
                         help="Client (application) secret, defaults to none")
-    parser.add_argument("--scope", metavar="SCOPE",
+    parser.add_argument("--scope", metavar="SCOPE", default=DEFAULT_OAUTH2_SCOPES,
                         dest='scopes', action='append',
-                        help="Authorization scope, defaults to 'https://outlook.office.com/IMAP.AccessAsUser.All, https://outlook.office.com/SMTP.Send'")
+                        help="Authorization scope, defaults to %s" % ', '.join(DEFAULT_OAUTH2_SCOPES))
     parser.add_argument("--cache", metavar="CACHE",
-                        dest='cache', default=None,
-                        help="Path to token cache file")
+                        dest='token_cache_file', default=None,
+                        help="Path to token cache file. Either --cache or --secret is allowed, not both.")
+    parser.add_argument("--secret",
+                        dest='secret', default=None,
+                        help="Name (title) of secret service entry containing the refresh token. Either --secret or --cache is allowed, not both.")
     parser.add_argument("--debug",
                         action="store_const", const=True, default=False,
                         help="Enable debugging")
@@ -209,11 +255,11 @@ if __name__ == '__main__':
     config = {
         'client_id' : options.client_id,
         'client_credential' : options.client_credential,
-        'scopes' : options.scopes if options.scopes else ["https://outlook.office.com/IMAP.AccessAsUser.All", "https://outlook.office.com/SMTP.Send"],
-        'cache' : options.cache
+        'scopes' : options.scopes,
+        'token_cache_file' : options.token_cache_file
     }
 
-    with MicrosoftO365(config) as provider:
+    with MicrosoftO365(**config) as provider:
         try:
             if options.imap_test:
                 import imaplib
@@ -224,6 +270,10 @@ if __name__ == '__main__':
             else:
                 if options.imap_auth_string is True:
                     output_string = provider.get_imap_auth_string()
+                elif options.secret is not None:
+                    if options.token_cache_file is not None:
+                        raise Exception("Either --secret or --cache is allowed, not both.")
+                    output_string = provider.get_token_by_refresh_token_in_secret(options.secret, sys.stdout.isatty())
                 else:
                     output_string = provider.get_token(sys.stdout.isatty())
 
